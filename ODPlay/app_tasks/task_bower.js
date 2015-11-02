bfbf27279576e2c9ddc49854762f24c2d9a7fb93
Shavin Fonseka <shavindra2001@yahoo.co.uk>
2015-11-02 17:04:35 +0000

    Updated the wirededp

--- ODPlay/app_tasks/task_bower.js
+++ ODPlay/app_tasks/task_bower.js
@@ -2,32 +2,120 @@
     wiredep = require('wiredep').stream,
     rename = require("gulp-rename"),
     rmdir = require('rimraf'),
-
+    tap = require('gulp-tap'),
+    fs = require('fs'),
+    path = require('path'),
+    run = require('gulp-run'),
     srcDir = 'src/app/assets/',
     distDir = 'dist/app/assets/';
 
-gulp.task('clean-assets', function () {
-    rmdir(distDir, function (error) { });
-})
+
+
+
+function renameDir(prefix, fileExt, suffix) {
+    var dir;
+    
+    switch (fileExt) {
+        
+        case '.js':
+            dir = 'js/libs/' + suffix;
+            break;
+
+        case '.css':
+            dir = 'styles/css/vendor/' + suffix;
+            break;
+            
+        case '.scss':
+            dir = 'styles/sass/vendor/' + suffix;
+            break;
+
+        case '.eot':
+            dir = 'fonts/';
+            break;
+
+        case '.woff':
+            dir = 'fonts/';
+            break;
+
+        case '.ttf':
+            dir = 'fonts/';
+            break;
+
+        case '.svg':
+            dir = 'fonts/';
+            break;
+
+        default:
+            dir = 'components/vendor/' + suffix;
+                
+    }
+    
+    
+    return dir;
+
+}
+
+gulp.task('node', function () {
+    
+    run('node -v').exec()  // prints "Hello World\n". 
+
+});
 
 //When Bower install runs it will add the file paths to the index
 gulp.task('bower-html', function () {
     gulp.src('./www/index.html')
       .pipe(wiredep({
-          ignorePath: "bower_components",
-          fileTypes: {
-              html: {
-                  block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
-                  detect: {
-                      js: /<script.*src=['"]([^'"]+)/gi,
-                      css: /<link.*href=['"]([^'"]+)/gi
-                  },
-                  replace: {
-                      js: '<script src="/assets/js/libs/{{filePath}}"></script>',
-                      css: '<link rel="stylesheet" href="/assets/styles/css/vendor/{{filePath}}" />'
-                  }
-              }
-          }
-      }));
-    //.pipe(gulp.dest('./dest'));
+        ignorePath: "bower_components/",
+        fileTypes: {
+            html: {
+                block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
+                detect: {
+                    js: /<script.*src=['"]([^'"]+)/gi,
+                    css: /<link.*href=['"]([^'"]+)/gi
+                },
+                replace: {
+                    js: function (filePath) {
+                        var pathObj = path.parse(filePath);                                     
+                        var newPath = srcDir + 'js/libs/' + pathObj.name + '/' + pathObj.base;
+                        return '<script src=' + newPath + '></script>';
+                    },
+                    css: function (filePath) {
+                        var pathObj = path.parse(filePath);
+                        var newPath = srcDir + 'styles/css/vendor/' + pathObj.name + '/' + pathObj.base;
+                        return '<script src=' + newPath + '></script>';
+                    },
+                }
+            }
+        }
+    }))
+    .pipe(gulp.dest('./dist'));
+
 });
+
+gulp.task('clean-assets', function () {
+    rmdir(distDir, function (error) { });
+})
+
+//Copy bower files from bower main directory to actual source directory
+gulp.task('bower-copy', function () {
+    
+    var bowerFileJson = {};
+    return gulp.src(['bower_components/**/*.{js,css,scss,ttf,woff,eot,svg}', '!bower_components/**/src/**/*.{js,css,scss,ttf,woff,eot,svg}'])
+
+    .pipe(rename(function (path) {
+        
+        var dirSuffix = path.dirname.indexOf("\\") > -1 ? path.dirname.split('\\')[0] : path.dirname;
+        path.dirname = renameDirPaths('', path.extname, dirSuffix);
+        
+        
+        return path;
+       
+    }))
+    .on('end', function () {
+        
+        fs.writeFile('bower.files.json')
+    })
+    .pipe(gulp.dest(srcDir));
+
+});
+
